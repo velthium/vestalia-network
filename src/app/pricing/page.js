8d94ea92b49521156d26db507d622feacd6d5009
Dark Velthium <dark-velthium@outlook.com>
2025-01-19 23:09:34 +0100

    Improve pricing page design
    - Change input background colors to improve readability
    - Update input positions for better layout
    - Add <hr /> tags to separate sections

--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -82,59 +82,63 @@ export default function Home() {
     <div className="container">
       <PageTitle title="Pricing" />
 
-      <div className="form-check text-start">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="storageUnit"
-          id="unit-mb"
-          value="MB"
-          checked={unit === 'MB'}
-          onChange={handleUnitChange}
-        />
-        <label className="form-check-label" htmlFor="unit-mb">
-          MB
-        </label>
-      </div>
-      <div className="form-check text-start">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="storageUnit"
-          id="unit-gb"
-          value="GB"
-          checked={unit === 'GB'}
-          onChange={handleUnitChange}
-        />
-        <label className="form-check-label" htmlFor="unit-gb">
-          GB
-        </label>
-      </div>
-      <div className="form-check text-start">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="storageUnit"
-          id="unit-tb"
-          value="TB"
-          checked={unit === 'TB'}
-          onChange={handleUnitChange}
-        />
-        <label className="form-check-label" htmlFor="unit-tb">
-          TB
-        </label>
+      <div className="d-flex justify-content-center">
+        <div className="form-check text-start">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="storageUnit"
+            id="unit-mb"
+            value="MB"
+            checked={unit === 'MB'}
+            onChange={handleUnitChange}
+          />
+          <label className="form-check-label" htmlFor="unit-mb">
+            MB
+          </label>
+        </div>
+        <div className="form-check text-start ms-3">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="storageUnit"
+            id="unit-gb"
+            value="GB"
+            checked={unit === 'GB'}
+            onChange={handleUnitChange}
+          />
+          <label className="form-check-label" htmlFor="unit-gb">
+            GB
+          </label>
+        </div>
+        <div className="form-check text-start ms-3">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="storageUnit"
+            id="unit-tb"
+            value="TB"
+            checked={unit === 'TB'}
+            onChange={handleUnitChange}
+          />
+          <label className="form-check-label" htmlFor="unit-tb">
+            TB
+          </label>
+        </div>
       </div>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      <div className="form-group mt-4">
+      <hr className="m-5" />
+      <div className="d-flex justify-content-evenly">
+      <div className="form-group">
         <label htmlFor="size-input" className="form-label">
           Enter Storage Size ({unit}):
         </label>
         <input
           type="number"
           id="size-input"
-          className="form-control"
+          className="form-control bg-sunshine w-75 m-auto"
           min={1}
           max={unit === 'MB' ? 999000 : 999}
           value={size}
@@ -142,22 +146,23 @@ export default function Home() {
         />
       </div>
 
-      <div className="form-group mt-4">
+      <div className="form-group">
         <label htmlFor="years-input" className="form-label">
           Enter Duration (Years):
         </label>
         <input
           type="number"
           id="years-input"
-          className="form-control"
+          className="form-control bg-sunshine w-75 m-auto"
           min="1"
           max="90"
           value={years}
           onChange={(e) => setYears(Number(e.target.value))}
         />
       </div>
-
-      <button className="btn mt-4 bg-warning" onClick={handlePurchase}>
+      </div>
+      <hr className='m-5' />
+      <button className="btn bg-warning" onClick={handlePurchase}>
         Purchase Storage Plan
       </button>
     </div>
